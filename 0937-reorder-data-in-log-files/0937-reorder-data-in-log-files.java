6932454d88796bf9060eaabf8fc7d182e09e5184
강태연 <67638005+taeyeon357@users.noreply.github.com>
2024-06-17 14:28:04 +0900

    Time: 5 ms (56.50%), Space: 44.6 MB (68.80%) - LeetHub

--- 0937-reorder-data-in-log-files/0937-reorder-data-in-log-files.java
+++ 0937-reorder-data-in-log-files/0937-reorder-data-in-log-files.java
@@ -1,19 +1,21 @@
 class Solution {
     public String[] reorderLogFiles(String[] logs) {
-        List<String[]> letter = new ArrayList<>();
-        List<String[]> digit = new ArrayList<>();
+        List<String> letter = new ArrayList<>();
+        List<String> digit = new ArrayList<>();
         String[] result = new String[logs.length];
         
         for(int i=0; i<logs.length; i++){
             String[] tmp = logs[i].split(" ",2);
             if(Character.isDigit(tmp[1].charAt(0))){
-                digit.add(tmp);
+                digit.add(logs[i]);
             }else{
-                letter.add(tmp);
+                letter.add(logs[i]);
             }
         }
         
-        Collections.sort(letter, (a, b) ->{
+        Collections.sort(letter, (s1, s2) ->{
+            String[] a = s1.split(" ", 2);
+            String[] b = s2.split(" ", 2);
             int cmp = a[1].compareTo(b[1]);
             if(cmp == 0){
                 return a[0].compareTo(b[0]);
@@ -23,11 +25,11 @@ class Solution {
         });
         
         for(int i=0; i<letter.size(); i++){
-            result[i] = String.join(" ", letter.get(i));
+            result[i] = letter.get(i);
         }
         
         for(int i=0; i<digit.size(); i++){
-            result[letter.size()+i] = String.join(" ", digit.get(i));
+            result[letter.size()+i] = digit.get(i);
         }
         
         return result;
