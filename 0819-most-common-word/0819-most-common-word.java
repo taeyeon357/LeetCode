dab26d599dff537cec0cd320910c3bb2900aa92a
강태연 <67638005+taeyeon357@users.noreply.github.com>
2024-06-17 16:19:55 +0900

    Time: 11 ms (85.99%), Space: 42.6 MB (85.48%) - LeetHub

--- 0819-most-common-word/0819-most-common-word.java
+++ 0819-most-common-word/0819-most-common-word.java
@@ -1,26 +1,16 @@
 class Solution {
     public String mostCommonWord(String paragraph, String[] banned) {
-        String[] paraWords = paragraph.replaceAll("\\W+", " ").toLowerCase().split(" ");
-        Map<String, Integer> map = new HashMap<>();
-        for(int i=0; i<paraWords.length; i++){
-            if(!map.containsKey(paraWords[i])){
-                map.put(paraWords[i], 1);
-            }else{
-                map.put(paraWords[i], map.get(paraWords[i])+1);
+        Set<String> ban = new HashSet<>(Arrays.asList(banned));
+        
+        Map<String, Integer> counts = new HashMap<>();
+        
+        String[] words = paragraph.replaceAll("\\W+", " ").toLowerCase().split(" ");
+        
+        for(String w : words){
+            if(!ban.contains(w)){
+                counts.put(w, counts.getOrDefault(w,0)+1);
             }
         }
-               
-        for(String ban : banned){
-            map.remove(ban);
-        }
-        int maxCnt = 0;
-        String result = ""; 
-        for(String key : map.keySet()){
-            if(maxCnt < map.get(key)){
-                maxCnt = map.get(key);
-                result = key;
-            }
-        }
-        return result;
+        return Collections.max(counts.entrySet(), Map.Entry.comparingByValue()).getKey();
     }
 }
