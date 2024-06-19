624540e427e8850b6138c45d0a01174f44639ab1
강태연 <67638005+taeyeon357@users.noreply.github.com>
2024-06-19 11:23:37 +0900

    Time: 3 ms (14.00%), Space: 45.6 MB (88.89%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,22 +1,22 @@
 class Solution {
     public int trap(int[] height) {
-        int l = 0;
-        int r = height.length - 1;
-        int leftMax = height[l];
-        int rightMax = height[r];
+        Deque<Integer> stk = new ArrayDeque<>();
+        int volume = 0;
         
-        int result = 0;
-        while(l<r){
-            leftMax = Math.max(leftMax, height[l]);
-            rightMax = Math.max(rightMax, height[r]);
-            
-            if(leftMax <= rightMax){
-                result += (leftMax - height[l++]);
-            }else{
-                result += (rightMax - height[r--]);
+        for(int i=0; i<height.length; i++){
+            while(!stk.isEmpty() && height[i] > height[stk.peek()]){
+                Integer top = stk.pop();
+                
+                if(stk.isEmpty()) break;
+                
+                int distance = i-stk.peek()-1;
+                int water = Math.min(height[i], height[stk.peek()])-height[top];
+                
+                volume += distance * water;
             }
+            stk.push(i);
+            
         }
-        
-        return result;
+        return volume;
     }
 }
