0bb97bbbe8a914bc2b311ec926a8a13ecaabd9e3
강태연 <67638005+taeyeon357@users.noreply.github.com>
2024-07-11 13:22:29 +0900

    Time: 1 ms (100.00%), Space: 44.5 MB (22.40%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.java
+++ 0002-add-two-numbers/0002-add-two-numbers.java
@@ -8,39 +8,36 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-import java.util.*;
-
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode node = new ListNode();
+        ListNode head = node;
         int carry = 0;
-        ListNode next1 = l1;
-        ListNode next2 = l2;
-        ListNode head = new ListNode();
-        ListNode cur = head;
-        while(next1 != null | next2 != null | carry==1){
-            int sum = (next1 != null ? next1.val : 0) + (next2 != null ? next2.val : 0);
-            if(carry==1){
-                sum += 1;
-                carry = 0;
-            }
-            
-            if(sum < 10) {
-                cur.next = new ListNode(sum);
-                cur = cur.next;
-            }else if(sum == 10){
-                cur.next = new ListNode(0);
-                cur = cur.next;
-                carry = 1;
+        while(true){
+            if(l1 != null && l2 != null){
+                node.next = new ListNode((l1.val + l2.val + carry)%10);
+                carry = (l1.val + l2.val + carry)/10;
+                l1 = l1.next;
+                l2 = l2.next; 
+            }else if(l1 != null){
+                node.next = new ListNode((l1.val + carry)%10);
+                carry = (l1.val + carry) / 10;
+                l1 = l1.next;
+            }else if(l2 != null){
+                node.next = new ListNode((l2.val + carry)%10);
+                carry = (l2.val + carry) / 10;
+                l2 = l2.next;
+            }else if(carry == 1){
+                node.next = new ListNode(1);
+                break;
             }else{
-                cur.next = new ListNode(sum%10);
-                cur = cur.next;
-                carry = 1;
+                break;
             }
+            node = node.next;
             
-            next1 = next1 == null ? null : next1.next;
-            next2 = next2 == null ? null : next2.next;
         }
-    
+
         return head.next;
+        
     }
 }
