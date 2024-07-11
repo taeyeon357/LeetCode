12b8831e261a12fa059bb911906a222e3049dc1a
강태연 <67638005+taeyeon357@users.noreply.github.com>
2024-07-11 12:43:33 +0900

    Time: 0 ms (100.00%), Space: 43.3 MB (7.84%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.java
+++ 0206-reverse-linked-list/0206-reverse-linked-list.java
@@ -10,17 +10,24 @@
  */
 class Solution {
     public ListNode reverseList(ListNode head) {
-        if(head != null){
+        if(head != null){ //빈 리스트가 아닐 경우 시작
+            //result에 node 추가
             ListNode result = new ListNode(head.val);
+            //다음 노드로 변경
             head = head.next;
+            //빈 리스트가 아니라면
             while(head != null){
+                //현재 노드를 저장
                 ListNode node = new ListNode(head.val);
+                //현재 노드의 next를 이전 노드로 지정
                 node.next = result;
+                //이전 노드를 현재 노드로 변경
                 result = node;
+                //다음 노드로 변경
                 head = head.next;
             }
-            
             return result;
+            
         }else{
             return null;
         }
